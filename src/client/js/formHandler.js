b08934d00be2a6d88458b9bc500736c9e574a9f6
aneeshgupta25 <aneeshgupta2512@gmail.com>
2023-06-26 03:24:01 +0530

    MeaningCloud API integration

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,40 @@
+const API_KEY = "323e96d222308b88af0e20ec237d9979"
+const base_url = "https://api.meaningcloud.com/sentiment-2.1"
+
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('name').value    
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:3000/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    console.log("::: Form Submitted :::")    
+    Client.getData({
+        text: "I am Aneesh & am a good boy!!!",
+        language: "en"
+    })    
+    .then(
+        function(data) {            
+            document.getElementById('results').innerHTML = data.subjectivity;
+        }
+    )
+    // fetch('http://localhost:3000/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
 }
 
-export { handleSubmit }
+const getData = async (data) => {    
+    const response = await fetch(`${base_url}?key=${API_KEY}&txt=${data.text}?lang=${data.language}`);
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    }
+    catch(e) {
+        console.log("Error Occurred while Fetching NLP data...");
+    }
+}
+
+export { handleSubmit, getData }
